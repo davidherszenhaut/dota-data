20bb2892d41a32bddad651e0101accb74f6b5398
David Herszenhaut <david.herszenhaut@gmail.com>
2018-06-03 21:51:47 -0400

    Began work on function to show/hide party and solo graphs

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var partyGraphs = document.getElementsByClassName("party");
+var soloGraphs = document.getElementsByClassName("solo");
+
+function hideGraphs(graphsArray) {
+    for (var i = 0; i < graphsArray.length; i++) {
+        graphsArray[i].style.display = "none";
+    }
+}
+
+hideGraphs(soloGraphs);
