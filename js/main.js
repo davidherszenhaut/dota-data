c07efa7f052896f2dbfa399adce2b2bc861f3300
David Herszenhaut <david.herszenhaut@gmail.com>
2018-06-04 01:14:03 -0400

    Added 'scroll to top' button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// PARTY QUEUE AND SOLO QUEUE TOGGLE SWITCH
+
 var partyGraphs = document.getElementsByClassName("party");
 var soloGraphs = document.getElementsByClassName("solo");
 
@@ -8,3 +10,22 @@ function hideGraphs(graphsArray) {
 }
 
 // hideGraphs(soloGraphs);
+
+// GO TO TOP BUTTON
+
+window.onscroll = function() {
+    scrollFunction();
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("go-to-top-btn").style.display = "block";
+    } else {
+        document.getElementById("go-to-top-btn").style.display = "none";
+    }
+}
+
+function goToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
